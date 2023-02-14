e5706259b3843d11a212a9cfb957ad5b9b3540c4
Dominik Obłoza <dev@dominikobloza.pl>
2023-02-14 22:54:54 +0100

    fix types problem

--- client/modules/outlays/components/CategoryItem.tsx
+++ client/modules/outlays/components/CategoryItem.tsx
@@ -4,35 +4,47 @@
  * User: @BLOCKYe
  * Date: 05/09/2022
  * Time: 23:38
-*/
+ */
 
-import React, {useEffect, useRef} from 'react';
-import {ICategoryData} from "../../categories/redux/CategoriesInterfaces";
-import {BsCheckLg} from "react-icons/bs";
-import autoAnimate from "@formkit/auto-animate";
+import React from "react";
+import type { ICategoryData } from "../../categories/redux/CategoriesInterfaces";
+import { BsCheckLg } from "react-icons/bs";
 import CategoryColors from "../../categories/utils/CategoryColors";
 
 interface ICategoryItem {
-    selectedCategories?: string[]
-    selectedCategory: (id: string) => void
-    data: ICategoryData
+  selectedCategories?: any;
+  selectedCategory: (id: string) => void;
+  data: ICategoryData;
 }
 
 const CategoryItem: React.FC<ICategoryItem> = (props) => {
+  /**
+   * This method is used to
+   * render styles
+   * for different variants
+   * @param color
+   */
 
-    /**
-     * This method is used to
-     * render styles
-     * for different variants
-     * @param color
-     */
-
-    return (
-        <div onClick={() => props.selectedCategory(props.data.id)} className={'py-1 hover:opacity-100 transition-all cursor-pointer px-3 font-bold rounded text-sm flex gap-2 items-center w-full ' + CategoryColors.ColorBuilder(props.data.color, "dark", "bg") + " " + CategoryColors.ColorBuilder(props.data.color, "default", "text") + " " + (props.selectedCategories && props.selectedCategories.includes(props.data.id) ? 'opacity-100' : 'opacity-50')}>
-            {props.selectedCategories && props.selectedCategories.includes(props.data.id) && <BsCheckLg />}
-            {props.data?.name}
-        </div>
-    );
+  return (
+    <div
+      onClick={() => props.selectedCategory(props.data.id)}
+      className={
+        "flex w-full cursor-pointer items-center gap-2 rounded py-1 px-3 text-sm font-bold transition-all hover:opacity-100 " +
+        CategoryColors.ColorBuilder(props.data.color, "dark", "bg") +
+        " " +
+        CategoryColors.ColorBuilder(props.data.color, "default", "text") +
+        " " +
+        (props.selectedCategories &&
+        props.selectedCategories.includes(props.data.id)
+          ? "opacity-100"
+          : "opacity-50")
+      }
+    >
+      {props.selectedCategories &&
+        props.selectedCategories.includes(props.data.id) && <BsCheckLg />}
+      {props.data?.name}
+    </div>
+  );
 };
 
 export default CategoryItem;
--- client/modules/outlays/components/OutlayModal.tsx
+++ client/modules/outlays/components/OutlayModal.tsx
@@ -4,196 +4,242 @@
  * User: @BLOCKYe
  * Date: 22.10.2022
  * Time: 23:27
-*/
+ */
 
-import React, {useState} from 'react';
-import {IOutlayData} from "../redux/OutlaysInterfaces";
+import React, { useState } from "react";
+import type { IOutlayData } from "../redux/OutlaysInterfaces";
 import {
-    Modal,
-    ModalBody,
-    ModalCloseButton,
-    ModalContent,
-    ModalFooter,
-    ModalHeader,
-    ModalOverlay
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
 } from "@chakra-ui/modal";
 import Input from "../../../common/components/inputs/Input";
 import Textarea from "../../../common/components/inputs/Textarea";
-import {ICategoryData} from "../../categories/redux/CategoriesInterfaces";
+import type { ICategoryData } from "../../categories/redux/CategoriesInterfaces";
 import CategoryItem from "./CategoryItem";
 import Button from "../../../common/components/buttons/Button";
-import {useDispatch, useSelector} from "react-redux";
-import {selectCategories} from "../../categories/redux/categoriesSlice";
-import {useFormik} from "formik";
-import {initialValues, outlaySchema} from "../utils/OutlayFormik";
+import { useDispatch, useSelector } from "react-redux";
+import { selectCategories } from "../../categories/redux/categoriesSlice";
+import { useFormik } from "formik";
+import { initialValues, outlaySchema } from "../utils/OutlayFormik";
 import moment from "moment/moment";
-import {selectLoading, setLoading} from "../../../common/redux/UISlice";
-import {deleteCategory, fetchCategories} from "../../categories/redux/CategoriesRepository";
-import {useToast} from "@chakra-ui/react";
-import {deleteOutlay, fetchOutlays} from "../redux/OutlaysRepository";
-import {fetchLastSpending} from "../../analytics/redux/AnalyticsRepository";
+import { selectLoading, setLoading } from "../../../common/redux/UISlice";
+import {
+  deleteCategory,
+  fetchCategories,
+} from "../../categories/redux/CategoriesRepository";
+import { useToast } from "@chakra-ui/react";
+import { deleteOutlay, fetchOutlays } from "../redux/OutlaysRepository";
+import { fetchLastSpending } from "../../analytics/redux/AnalyticsRepository";
 
 interface IOutlayModalProps {
-    isOpen: boolean,
-    onClose: () => void;
-    submitForm: (values: any) => void;
-    data?: IOutlayData
-    setPreview?: () => void;
+  isOpen: boolean;
+  onClose: () => void;
+  submitForm: (values: any) => void;
+  data?: IOutlayData;
+  setPreview?: () => void;
 }
 
 const OutlayModal: React.FC<IOutlayModalProps> = (props) => {
-    const dispatch: any = useDispatch()
-    const toast = useToast()
-    const [submitRemove, setSubmitRemove] = useState<boolean>(false)
-    const categories = useSelector(selectCategories)
-    const loading = useSelector(selectLoading)
+  const dispatch: any = useDispatch();
+  const toast = useToast();
+  const [submitRemove, setSubmitRemove] = useState<boolean>(false);
+  const categories = useSelector(selectCategories);
+  const loading = useSelector(selectLoading);
 
-    // create formik instance
-    const formik = useFormik({
-        validateOnChange: false,
-        validateOnBlur: false,
-        enableReinitialize: true,
-        initialValues: {
-            date: props.data?.date || moment(new Date()).format("yyyy-MM-DD"),
-            title: props.data?.title || '',
-            description: props.data?.description || '',
-            value: props.data?.value || 0,
-            categories: props.data?.categories && props.data?.categories.length > 0 ? [props.data?.categories[0].id] : [] || []
-        },
-        validationSchema: outlaySchema,
-        onSubmit: async (values, {resetForm}) => {
-            await props.submitForm(values);
-            props.setPreview && props.setPreview()
-            resetForm()
-        }
-    })
+  // create formik instance
+  const formik = useFormik({
+    validateOnChange: false,
+    validateOnBlur: false,
+    enableReinitialize: true,
+    initialValues: {
+      date: props.data?.date || moment(new Date()).format("yyyy-MM-DD"),
+      title: props.data?.title || "",
+      description: props.data?.description || "",
+      value: props.data?.value || 0,
+      categories:
+        props.data?.categories && props.data?.categories.length > 0
+          ? [props.data?.categories[0]?.id]
+          : [] || [],
+    },
+    validationSchema: outlaySchema,
+    onSubmit: async (values, { resetForm }) => {
+      await props.submitForm(values);
+      props.setPreview && props.setPreview();
+      resetForm();
+    },
+  });
 
+  /**
+   *
+   * @param id
+   */
 
-    /**
-     *
-     * @param id
-     */
+  const removeOutlay = async (id?: string): Promise<void> => {
+    if (!id) return;
 
-    const removeOutlay = async (id?: string): Promise<void> => {
-        if (!id) return
+    setSubmitRemove(true);
+    if (!submitRemove) return;
 
-        setSubmitRemove(true)
-        if (!submitRemove) return
+    try {
+      await dispatch(setLoading(true));
 
-        try {
-            await dispatch(setLoading(true))
+      await dispatch(deleteOutlay(id));
 
-            await dispatch(deleteOutlay(id))
+      const promises = [
+        dispatch(fetchOutlays()),
+        dispatch(fetchLastSpending()),
+      ];
 
-            const promises = [
-                dispatch(fetchOutlays()),
-                dispatch(fetchLastSpending())
-            ]
+      await Promise.all(promises);
 
-            await Promise.all(promises)
+      toast({
+        title: `Usunięto wydatek: ${props?.data?.title}`,
+        status: "info",
+        isClosable: true,
+      });
 
-            toast({
-                title: `Usunięto wydatek: ${props?.data?.title}`,
-                status: 'info',
-                isClosable: true
-            })
+      await dispatch(setLoading(false));
+    } catch (e: any) {
+      toast({
+        title: e?.message,
+        status: "error",
+      });
 
-            await dispatch(setLoading(false))
-        } catch (e: any) {
-            toast({
-                title: e?.message,
-                status: 'error'
-            })
-
-            await dispatch(setLoading(false))
-        }
+      await dispatch(setLoading(false));
     }
+  };
 
-    return (
-        <Modal onClose={() => {
-            props.onClose();
-            props.setPreview && props.setPreview()
-        }} isOpen={props.isOpen}>
-            <ModalOverlay/>
-            <ModalContent>
-                <ModalHeader className={'bg-d'}>{props.data?.id ? 'Edytuj wydatek' : 'Dodaj nowy wydatek'}</ModalHeader>
-                <ModalCloseButton/>
+  return (
+    <Modal
+      onClose={() => {
+        props.onClose();
+        props.setPreview && props.setPreview();
+      }}
+      isOpen={props.isOpen}
+    >
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader className={"bg-d"}>
+          {props.data?.id ? "Edytuj wydatek" : "Dodaj nowy wydatek"}
+        </ModalHeader>
+        <ModalCloseButton />
 
-                {/* <--- Form ---> */}
-                <ModalBody className={'bg-d'}>
-                    <div className={'text-w-darker'}>
-                        Aby utworzyć nowy dodatek wypełnij formularz. Wprowadź tytuł, kwotę oraz datę.
-                    </div>
+        {/* <--- Form ---> */}
+        <ModalBody className={"bg-d"}>
+          <div className={"text-w-darker"}>
+            Aby utworzyć nowy dodatek wypełnij formularz. Wprowadź tytuł, kwotę
+            oraz datę.
+          </div>
 
-                    <form className={'grid gap-5 mt-3'} onSubmit={formik.handleSubmit}>
-                        <Input onChange={formik.handleChange} value={formik.values.title} name={'title'}
-                            err={formik.errors.title} type={'text'} label={'Tytuł wydatku'}
-                            placeholder={'Tytuł wydatku'}/>
+          <form className={"mt-3 grid gap-5"} onSubmit={formik.handleSubmit}>
+            <Input
+              onChange={formik.handleChange}
+              value={formik.values.title}
+              name={"title"}
+              err={formik.errors.title}
+              type={"text"}
+              label={"Tytuł wydatku"}
+              placeholder={"Tytuł wydatku"}
+            />
 
-                        <Textarea onChange={formik.handleChange} value={formik.values.description}
-                            name={'description'} label={'Opis'} placeholder={'Opis'}
-                            err={formik.errors.description}/>
+            <Textarea
+              onChange={formik.handleChange}
+              value={formik.values.description}
+              name={"description"}
+              label={"Opis"}
+              placeholder={"Opis"}
+              err={formik.errors.description}
+            />
 
-                        <Input onChange={formik.handleChange} value={formik.values.value} name={'value'}
-                            type={'number'} label={'Kwota w PLN'} placeholder={'Kwota w PLN'}
-                            err={formik.errors.value}/>
+            <Input
+              onChange={formik.handleChange}
+              value={formik.values.value}
+              name={"value"}
+              type={"number"}
+              label={"Kwota w PLN"}
+              placeholder={"Kwota w PLN"}
+              err={formik.errors.value}
+            />
 
-                        <Input onChange={formik.handleChange} value={formik.values.date} name={'date'}
-                            type={'date'} label={'Data wydatku'} placeholder={'Data wydatku'}
-                            err={formik.errors.date}/>
+            <Input
+              onChange={formik.handleChange}
+              value={formik.values.date}
+              name={"date"}
+              type={"date"}
+              label={"Data wydatku"}
+              placeholder={"Data wydatku"}
+              err={formik.errors.date}
+            />
 
+            {/* <--- Categories ---> */}
+            <div>
+              <div className={"text-sm text-w-darker"}>Wybierz kategorie</div>
 
-                        {/* <--- Categories ---> */}
-                        <div>
-                            <div className={'text-sm text-w-darker'}>
-                                Wybierz kategorie
-                            </div>
+              <div className={"mt-2 flex flex-wrap items-center gap-2"}>
+                {categories &&
+                  categories.map((category: ICategoryData) => (
+                    <CategoryItem
+                      selectedCategories={formik.values.categories}
+                      key={category.id}
+                      selectedCategory={() =>
+                        formik.setFieldValue("categories", [category.id])
+                      }
+                      data={category}
+                    />
+                  ))}
+              </div>
 
-                            <div className={'flex-wrap flex gap-2 items-center mt-2'}>
-                                {categories && categories.map((category: ICategoryData) => (
-                                    <CategoryItem selectedCategories={formik.values.categories} key={category.id}
-                                        selectedCategory={() => formik.setFieldValue('categories', [category.id])}
-                                        data={category}/>
-                                ))}
-                            </div>
+              {categories && categories.length === 0 && (
+                <div className={"mt-1 text-sm text-pink-600"}>
+                  Nie utworzyłeś jeszcze żadnych kategorii.
+                </div>
+              )}
+            </div>
 
-                            {categories && categories.length === 0 && (
-                                <div className={'text-sm text-pink-600 mt-1'}>
-                                    Nie utworzyłeś jeszcze żadnych kategorii.
-                                </div>
-                            )}
-                        </div>
+            <ModalFooter className={"flex gap-3"}>
+              <Button
+                variant={"OUTLINED"}
+                text={"Anuluj"}
+                onClick={() => {
+                  props.onClose();
+                  props.setPreview && props.setPreview();
+                }}
+              />
+              <Button
+                variant={"CONTAINED"}
+                text={"Zapisz"}
+                type={"submit"}
+                disabled={!formik.dirty || loading}
+              />
+            </ModalFooter>
 
-                        <ModalFooter className={'flex gap-3'}>
-                            <Button variant={'OUTLINED'} text={'Anuluj'} onClick={() => {
-                                props.onClose();
-                                props.setPreview && props.setPreview()
-                            }}/>
-                            <Button variant={'CONTAINED'} text={'Zapisz'} type={'submit'}
-                                disabled={!formik.dirty || loading}/>
-                        </ModalFooter>
+            {/* <--- Delete outlay confirmation ---> */}
+            <>
+              {submitRemove && (
+                <div className={"mt-1 text-sm text-pink-600"}>
+                  Czy na pewno chcesz usunać ten wydatek?
+                </div>
+              )}
 
-
-                        {/* <--- Delete outlay confirmation ---> */}
-                        <>
-                            {submitRemove && (
-                                <div className={'text-sm text-pink-600 mt-1'}>
-                                    Czy na pewno chcesz usunać ten wydatek?
-                                </div>
-                            )}
-
-                            {props.data?.id && (
-                                <Button variant={'OUTLINED'} disabled={loading}
-                                    text={submitRemove ? 'Tak, usuń!' : 'Usuń wydatek'}
-                                    onClick={() => removeOutlay(props.data?.id)}/>
-                            )}
-                        </>
-                    </form>
-                </ModalBody>
-
-            </ModalContent>
-        </Modal>
-    );
+              {props.data?.id && (
+                <Button
+                  variant={"OUTLINED"}
+                  disabled={loading}
+                  text={submitRemove ? "Tak, usuń!" : "Usuń wydatek"}
+                  onClick={() => removeOutlay(props.data?.id)}
+                />
+              )}
+            </>
+          </form>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
 };
 
 export default OutlayModal;
