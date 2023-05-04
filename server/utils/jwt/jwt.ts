3ddd9388f5b9d9f53270fd975e74a2370bc0b690
BLOCKYe <dev@dominikobloza.pl>
2023-05-04 15:22:39 +0200

    fix asynchronicity problem of mail service

--- server/utils/jwt/jwt.ts
+++ server/utils/jwt/jwt.ts
@@ -4,78 +4,76 @@
  * User: @BLOCKYe
  * Date: 12/08/2022
  * Time: 00:19
-*/
+ */
 
-import {User} from "../../../generated-interfaces";
-import { verify, sign } from 'jsonwebtoken'
-import {IGenerateTokens} from "./IJwt";
+import { sign } from "jsonwebtoken";
+import type { IGenerateTokens } from "./IJwt";
 import * as crypto from "crypto";
+import type { User } from ".prisma/client";
 
 export default class Jwt {
+  /**
+   * This method is used to
+   * generate access token
+   * @param user
+   */
 
-    /**
-     * This method is used to
-     * generate access token
-     * @param user
-     */
+  private generateAccessToken(user: User): string | undefined {
+    const JWTSecretKey: string | undefined = process.env.JWT_ACCESS_SECRET;
+    if (!JWTSecretKey) return;
 
-    private generateAccessToken(user: User): string | undefined {
+    return sign({ userId: user.id }, JWTSecretKey, {
+      expiresIn: "24h",
+    });
+  }
 
-        const JWTSecretKey: string | undefined = process.env.JWT_ACCESS_SECRET
-        if(!JWTSecretKey) return
+  /**
+   * This method is used to
+   * generate refresh token
+   * @param user
+   * @param jti
+   */
 
-        return sign({ userId: user.id }, JWTSecretKey, {
-            expiresIn: '24h',
-        });
-    }
+  private generateRefreshToken(user: User, jti: string): string | undefined {
+    const JWTRefreshKey: string | undefined = process.env.JWT_REFRESH_SECRET;
+    if (!JWTRefreshKey) return;
 
+    return sign(
+      {
+        userId: user.id,
+        jti,
+      },
+      JWTRefreshKey,
+      {
+        expiresIn: "8h",
+      }
+    );
+  }
 
-    /**
-     * This method is used to
-     * generate refresh token
-     * @param user
-     * @param jti
-     */
+  /**
+   * This method is used to
+   * generate tokens
+   * @param user
+   * @param jti
+   */
 
-    private generateRefreshToken(user: User, jti: string): string | undefined {
+  public generateTokens(user: User, jti: string): IGenerateTokens {
+    const accessToken = this.generateAccessToken(user);
+    const refreshToken = this.generateRefreshToken(user, jti);
 
-        const JWTRefreshKey: string | undefined = process.env.JWT_REFRESH_SECRET
-        if(!JWTRefreshKey) return
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
 
-        return sign({
-            userId: user.id,
-            jti
-        }, JWTRefreshKey, {
-            expiresIn: '8h',
-        });
-    }
+  /**
+   * This method is used to
+   * hash token
+   * @param token
+   */
 
-
-    /**
-     * This method is used to
-     * generate tokens
-     * @param user
-     * @param jti
-     */
-
-    public generateTokens(user: User, jti: string): IGenerateTokens {
-        const accessToken = this.generateAccessToken(user)
-        const refreshToken = this.generateRefreshToken(user, jti)
-
-        return {
-            accessToken,
-            refreshToken
-        }
-    }
-
-
-    /**
-     * This method is used to
-     * hash token
-     * @param token
-     */
-
-    public hashToken(token: string): string {
-        return crypto.createHash('sha512').update(token).digest('hex');
-    }
+  public hashToken(token: string): string {
+    return crypto.createHash("sha512").update(token).digest("hex");
+  }
 }
