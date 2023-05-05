4459a607d95d1d3a332af531dd0cb24f74bf6792
BLOCKYe <dev@dominikobloza.pl>
2023-05-05 02:52:30 +0200

    implement validators in categories.service.ts

--- server/modules/categories/ICategories.ts
+++ server/modules/categories/ICategories.ts
@@ -1,10 +1,14 @@
+import type { Category } from ".prisma/client";
+
 export interface ICategoryCreateData {
-    name: string,
-    color: string,
-    userId: string
+  name: string;
+  color: string;
+  userId: string;
 }
 
 export interface ICategoryEditData {
-    name?: string,
-    color?: string
+  name?: string;
+  color?: string;
 }
+
+export type ICategory = Category | null;
